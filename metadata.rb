44278545fa6198e0e52e41041eedec169c8cafb3
Michael Burns <michael@mirwin.net>
2017-10-13 15:09:54 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.4'
+version '0.2.5'
 
 depends 'ele-apt'
 depends 'iptables'
