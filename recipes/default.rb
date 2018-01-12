18bc83e51cf8c108bb7366302a15fc7366798d08
Michael Burns <michael@mirwin.net>
2018-01-12 12:26:31 -0800

    bump to ES 5.6.5

--- recipes/default.rb
+++ recipes/default.rb
@@ -41,7 +41,7 @@ Chef::Log.debug("\e[32mElasticsearch:\e[33m computed seed_nodes: #{seed_nodes}.\
 include_recipe 'ele-apt'
 
 package 'elasticsearch' do
-  version '1.7.3'
+  version '5.6.5'
 end
 
 directory '/etc/elasticsearch' do
